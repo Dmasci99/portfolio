27d2a6a312b8d3d1ac6a882dc775ee8a8674e632
Dan Masci <minimasci@yahoo.ca>
2015-12-01 22:37:05 -0500

    Customize new drop-down menu for Dashboard
    - Wow these commits are all messed up. I just failed big time.
    
    DO NOT LISTEN TO COMMIT TITLES. Look at files.

--- server/models/todo.js
+++ server/models/todo.js
@@ -4,11 +4,26 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var TodoSchema = new Schema({
-	name: String,
-	completed: Boolean,
-	note: String,
-	username: String,
-	updated: {type: Date, default: Date.now}
+	name: {
+		type: String,
+		trim: true
+	},
+	note: {
+		type: String,
+		trim: true
+	},
+	username: {
+		type: String,
+		trim: true
+	},
+	completed: {
+		type: Boolean,
+		default: true
+	},
+	updated: {
+		type: Date, 
+		default: Date.now
+	}
 }, {
 	collection: 'todos'
 });
